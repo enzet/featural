085971c94b4ce519867fd62ef06e1afde3d7d110
Sergey Vartanov <me@enzet.ru>
2024-07-30 01:41:31 +0400

    Remove intermediate TeX files
    Now we create the whole TeX file from Moire code without any including
    and embedding.

--- build.sh
+++ build.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 
 BUILD_DIRECTORY="build"
+OUTPUT_DIRECTORY="out"
+
+# Clear everything.
+rm -rf ${BUILD_DIRECTORY}
+rm -rf ${OUTPUT_DIRECTORY}
 
 # Stop on error.
 set -e
@@ -10,23 +15,18 @@ clang-format --dry-run --Werror src/main.cpp
 black --check --line-length 80 python/main.py
 
 # Configure C++ code.
-rm -rf ${BUILD_DIRECTORY}
-mkdir ${BUILD_DIRECTORY}
+mkdir -p ${BUILD_DIRECTORY}
 cmake . -B ${BUILD_DIRECTORY}
 
 # Build C++ code into a `language` executable.
 cmake --build build
 
-# Construct TeX file.
-mkdir -p out
-
-build/language table \
-    "labial,labial;dental,dental,alveolar,postalveolar,retroflex,palatal,velar,uvular,pharyngeal,glottal" \
-    "plosive;voiceless,plosive;voiced,nasal;voiceless,nasal;voiced,trill;voiceless,trill;voiced,tap/flap;voiceless,tap/flap;voiced,sibilant_fricative;voiceless,sibilant_fricative;voiced,non-sibilant_fricative;voiceless,non-sibilant_fricative;voiced,lateral_fricative;voiceless,lateral_fricative;voiced,approximant;voiced,lateral_approximant;voiceless,lateral_approximant;voiced" \
-    > out/ipa.tex
-cp data/*.tex out/
+# Construct TeX file, `moire_converter` will use `language` executable
+# internally.
+mkdir -p ${OUTPUT_DIRECTORY}
+python python/moire_converter.py \
+    --input data/text.moi --output ${OUTPUT_DIRECTORY}/text.tex --format tex
 
 # Construct PDF file.
 cd out
-xelatex table.tex
 xelatex text.tex
--- python/moire_converter.py
+++ python/moire_converter.py
@@ -1,10 +1,34 @@
-from moire.default import DefaultTeX
+from moire.default import Default, DefaultTeX
+from moire.main import main
 from pathlib import Path
+from textwrap import dedent
 import subprocess
 import sys
 
 
-class LanguageTeX(DefaultTeX):
+class Language(Default):
+    pass
+
+
+class LanguageTeX(Language, DefaultTeX):
+    def body(self, arg) -> str:
+        s = dedent(
+            """\
+            \\documentclass[tikz]{article}
+            \\usepackage[utf8]{inputenc}
+            \\usepackage{tikz}
+            \\usepackage{fontspec}
+            \\usepackage[left=1.25in,right=1.25in,top=1in,bottom=1in]{geometry}
+            \\setmainfont[Ligatures=TeX]{CMU Serif}
+            \\newfontface\doulos{Doulos SIL}
+            \\usetikzlibrary{positioning}
+            \\begin{document}
+            """
+        )
+        s += self.parse(arg[0], in_block=True)
+        s += "\\end {document}"
+        return s
+
     def ipa(self, arg) -> str:
         return f"{{\\doulos{{{self.parse(arg[0])}}}}}"
 
@@ -16,8 +40,8 @@ class LanguageTeX(DefaultTeX):
         return proc.stdout.read().decode()
 
     def symbol_table(self, arg) -> str:
-        rows = ",".join([self.clear(x) for x in arg[0]])
-        columns = ",".join([self.clear(x) for x in arg[1]])
+        rows: str = ",".join([self.clear(x) for x in arg[0]])
+        columns: str = ",".join([self.clear(x) for x in arg[1]])
         proc = subprocess.Popen(
             ["build/language", "table", rows, columns],
             stdout=subprocess.PIPE,
@@ -26,8 +50,4 @@ class LanguageTeX(DefaultTeX):
         
 
 if __name__ == "__main__":
-    with Path(sys.argv[1]).open() as input_file:
-        with Path(sys.argv[2]).open("w") as output_file:
-            output_file.write(
-                LanguageTeX().convert(input_file.read(), wrap=False)
-            )
+    main(sys.argv[1:], Language)
